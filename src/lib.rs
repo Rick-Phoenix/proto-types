9e621787b40a6d63b74968c4298a183470669ab9
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2026-01-14 18:52:20 +0100

    feat: no_std support

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,18 @@
+// no_std support is planned at a certain point, but not yet implemented
+#![no_std]
+#![deny(clippy::alloc_instead_of_core)]
+#![deny(clippy::std_instead_of_alloc)]
+#![deny(clippy::std_instead_of_core)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc = include_str!("../README.md")]
 
 //! # Feature flags
 #![doc = document_features::document_features!()]
+
+#[cfg(any(test, feature = "std"))]
+extern crate std;
+
+extern crate alloc;
 
 /// Generated rust code from buf.validate protobuf package, with some added methods and structs.
 #[cfg(feature = "protovalidate")]
@@ -50,13 +60,14 @@ mod conversions;
 mod datetime_internal;
 mod type_url;
 
+use alloc::{format, string::String, string::ToString, vec::Vec};
+use core::str::FromStr;
 use core::{convert::TryFrom, fmt, time};
-use std::str::FromStr;
-
-use prost::{
-  DecodeError, EncodeError, Message, Name,
-  alloc::{format, string::String, vec::Vec},
+use core::{
+  fmt::{Debug, Display},
+  hash::Hash,
 };
+<<<<<<< HEAD
 <<<<<<< HEAD
 pub(crate) use type_url::{type_url_for, TypeUrl};
 <<<<<<< HEAD
@@ -368,5 +379,10 @@ pub struct Status {
 ||||||| parent of c3ae3b1 (feat(types): added int wrappers)
 pub(crate) use type_url::{type_url_for, TypeUrl};
 =======
+||||||| parent of 4eae320 (feat(types): no_std support)
+=======
+
+use prost::{DecodeError, EncodeError, Message, Name};
+>>>>>>> 4eae320 (feat(types): no_std support)
 pub(crate) use type_url::{TypeUrl, type_url_for};
 >>>>>>> c3ae3b1 (feat(types): added int wrappers)
