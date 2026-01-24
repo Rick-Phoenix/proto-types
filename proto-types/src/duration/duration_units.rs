9eb88967815b8859eff7f18bc34ac4aeb7c525d9
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2026-01-24 19:26:15 +0100

    refactor!: use idiomatic Display impl for Duration units

--- proto-types/src/duration/duration_units.rs
+++ proto-types/src/duration/duration_units.rs
@@ -1,4 +1,22 @@
-use crate::{String, format};
+use alloc::string::ToString;
+
+use crate::String;
+
+macro_rules! impl_display {
+  ($($name:ident),*) => {
+    paste::paste! {
+      $(
+        impl core::fmt::Display for [< $name s >] {
+          fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+            write!(f, "{} {}{}", self.value, stringify!([< $name:lower >]), if self.value != 1 { "s" } else { "" })
+          }
+        }
+      )*
+    }
+  };
+}
+
+impl_display!(Second, Minute, Hour, Day, Week, Month, Year);
 
 /// A struct representing seconds. Wraps the value and provides extra formatting methods.
 #[derive(Debug, Default, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -7,27 +25,13 @@ pub struct Seconds {
 }
 
 impl Seconds {
-  /// Returns a string displaying the amount of seconds (e.g. "1 second", "2 seconds")
-  #[must_use]
-  pub fn format(&self) -> String {
-    format!(
-      "{} second{}",
-      self.value,
-      if self.value == 1 { "" } else { "s" }
-    )
-  }
-
   /// Returns a string with the amount of seconds, but only if the amount is more than 0.
   #[must_use]
   pub fn format_if_nonzero(&self) -> Option<String> {
     if self.is_zero() {
       return None;
     }
-    Some(format!(
-      "{} second{}",
-      self.value,
-      if self.value == 1 { "" } else { "s" }
-    ))
+    Some(self.to_string())
   }
 
   const fn is_zero(&self) -> bool {
@@ -43,26 +47,13 @@ pub struct Minutes {
 
 /// Returns a string displaying the amount of minutes (e.g. "1 minute", "2 minutes")
 impl Minutes {
-  #[must_use]
-  pub fn format(&self) -> String {
-    format!(
-      "{} minute{}",
-      self.value,
-      if self.value == 1 { "" } else { "s" }
-    )
-  }
-
   /// Returns a string with the amount of minutes, but only if the amount is more than 0.
   #[must_use]
   pub fn format_if_nonzero(&self) -> Option<String> {
     if self.is_zero() {
       return None;
     }
-    Some(format!(
-      "{} minute{}",
-      self.value,
-      if self.value == 1 { "" } else { "s" }
-    ))
+    Some(self.to_string())
   }
 
   const fn is_zero(&self) -> bool {
@@ -78,26 +69,13 @@ pub struct Hours {
 
 /// Returns a string displaying the amount of hours (e.g. "1 hour", "2 hours")
 impl Hours {
-  #[must_use]
-  pub fn format(&self) -> String {
-    format!(
-      "{} hour{}",
-      self.value,
-      if self.value == 1 { "" } else { "s" }
-    )
-  }
-
   /// Returns a string with the amount of hours, but only if the amount is more than 0.
   #[must_use]
   pub fn format_if_nonzero(&self) -> Option<String> {
     if self.is_zero() {
       return None;
     }
-    Some(format!(
-      "{} hour{}",
-      self.value,
-      if self.value == 1 { "" } else { "s" }
-    ))
+    Some(self.to_string())
   }
 
   const fn is_zero(&self) -> bool {
@@ -113,26 +91,13 @@ pub struct Days {
 
 /// Returns a string displaying the amount of days (e.g. "1 day", "2 days")
 impl Days {
-  #[must_use]
-  pub fn format(&self) -> String {
-    format!(
-      "{} day{}",
-      self.value,
-      if self.value == 1 { "" } else { "s" }
-    )
-  }
-
   /// Returns a string with the amount of days, but only if the amount is more than 0.
   #[must_use]
   pub fn format_if_nonzero(&self) -> Option<String> {
     if self.is_zero() {
       return None;
     }
-    Some(format!(
-      "{} day{}",
-      self.value,
-      if self.value == 1 { "" } else { "s" }
-    ))
+    Some(self.to_string())
   }
 
   const fn is_zero(&self) -> bool {
@@ -147,27 +112,13 @@ pub struct Weeks {
 }
 
 impl Weeks {
-  /// Returns a string displaying the amount of weeks (e.g. "1 week", "2 weeks")
-  #[must_use]
-  pub fn format(&self) -> String {
-    format!(
-      "{} week{}",
-      self.value,
-      if self.value == 1 { "" } else { "s" }
-    )
-  }
-
   /// Returns a string with the amount of weeks, but only if the amount is more than 0.
   #[must_use]
   pub fn format_if_nonzero(&self) -> Option<String> {
     if self.is_zero() {
       return None;
     }
-    Some(format!(
-      "{} week{}",
-      self.value,
-      if self.value == 1 { "" } else { "s" }
-    ))
+    Some(self.to_string())
   }
 
   /// Returns `true` if the value is zero.
@@ -183,27 +134,13 @@ pub struct Months {
 }
 
 impl Months {
-  /// Returns a string displaying the amount of months (e.g. "1 month", "2 months")
-  #[must_use]
-  pub fn format(&self) -> String {
-    format!(
-      "{} month{}",
-      self.value,
-      if self.value == 1 { "" } else { "s" }
-    )
-  }
-
   /// Returns a string with the amount of months, but only if the amount is more than 0.
   #[must_use]
   pub fn format_if_nonzero(&self) -> Option<String> {
     if self.is_zero() {
       return None;
     }
-    Some(format!(
-      "{} month{}",
-      self.value,
-      if self.value == 1 { "" } else { "s" }
-    ))
+    Some(self.to_string())
   }
 
   const fn is_zero(&self) -> bool {
@@ -218,27 +155,13 @@ pub struct Years {
 }
 
 impl Years {
-  /// Returns a string displaying the amount of years (e.g. "1 year", "2 years")
-  #[must_use]
-  pub fn format(&self) -> String {
-    format!(
-      "{} year{}",
-      self.value,
-      if self.value == 1 { "" } else { "s" }
-    )
-  }
-
   /// Returns a string with the amount of years, but only if the amount is more than 0.
   #[must_use]
   pub fn format_if_nonzero(&self) -> Option<String> {
     if self.is_zero() {
       return None;
     }
-    Some(format!(
-      "{} year{}",
-      self.value,
-      if self.value == 1 { "" } else { "s" }
-    ))
+    Some(self.to_string())
   }
 
   const fn is_zero(&self) -> bool {
