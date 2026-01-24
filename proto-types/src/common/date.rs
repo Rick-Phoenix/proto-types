be6c6d9eef04093f0e5c31c1a589c4efad2680db
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2026-01-24 16:14:44 +0100

    fix: corrected faulty logic for DateTime -> FixedOffset conversions

--- proto-types/src/common/date.rs
+++ proto-types/src/common/date.rs
@@ -193,7 +193,7 @@ const fn is_leap_year(year: i32) -> bool {
   (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))
 }
 
-const fn days_in_month(month: i32, year: i32) -> i32 {
+pub(crate) const fn days_in_month(month: i32, year: i32) -> i32 {
   match month {
     1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
     4 | 6 | 9 | 11 => 30,
