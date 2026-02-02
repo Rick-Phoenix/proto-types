0411867ec506042d6d26e1db3ad34c171fa3205d
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2026-02-02 20:20:24 +0100

    updated pre-release workflow

--- pre_release.sh
+++ pre_release.sh
@@ -4,23 +4,29 @@ set -eo pipefail
 
 EXEC_RELEASE=false
 if [[ "${2:-}" == "--execute" ]]; then
-  EXEC_RELEASE=true
+	EXEC_RELEASE=true
 fi
 VERSION="$1"
 
 if [[ -z "$VERSION" ]]; then
-  echo "Missing new version"
-  exit 1
+	echo "Missing new version"
+	exit 1
 fi
 
+git cliff
+
 if [[ "$EXEC_RELEASE" = true ]]; then
-  echo "Starting pre-release process for version ${VERSION}..."
+	echo "Starting pre-release process for version ${VERSION}..."
 
-  echo "Generating changelog..."
-  git cliff --tag "$VERSION" -o "CHANGELOG.md"
+	echo "Generating changelog..."
+	git cliff --tag "$VERSION" -o "CHANGELOG.md"
 
-  git add "CHANGELOG.md"
+	git add "CHANGELOG.md"
 
-  echo "Committing the new changelog"
-  git commit -m "updated changelog"
+	if [[ -n $(git status --porcelain "CHANGELOG.md") ]]; then
+		echo "Committing the new changelog..."
+		git commit -m "chore(release): update changelog for ${VERSION}"
+	else
+		echo "No changes in changelog."
+	fi
 fi
